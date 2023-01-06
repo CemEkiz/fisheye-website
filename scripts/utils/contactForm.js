1c28a91611509fd40dfa1444812e824f24f906ca
Cem Ekiz <ekizcem.pro@gmail.com>
2023-01-06 20:03:22 +0100

    add inputs to the contact form and improve the overlay color + more clean code

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,14 @@
-const modal = document.getElementById('contact_modal');
+const modal = document.querySelector('.modal');
+const form = document.querySelector('.form');
+const overlay = document.querySelector('.overlay');
 
 function displayModal() {
-  modal.style.display = 'block';
+  modal.style.display = 'flex';
+  overlay.style.display = 'block';
 }
 
 function closeModal() {
   modal.style.display = 'none';
+  overlay.style.display = 'none';
+  form.reset();
 }
