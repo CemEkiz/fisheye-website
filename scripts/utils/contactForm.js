a77bd9f9b4fc35f8a283898afac5c4f580349162
Cem Ekiz <ekizcem.pro@gmail.com>
2023-01-08 18:48:58 +0100

    add form validation

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -12,3 +12,68 @@ function closeModal() {
   overlay.style.display = 'none';
   form.reset();
 }
+
+// Form Validation
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+
+const checkFirstName = () => {
+  const firstNameValue = firstName.value.trim();
+
+  if (firstNameValue === '') {
+    return false;
+  } else {
+    console.log(firstNameValue);
+    return true;
+  }
+};
+
+const checkLastName = () => {
+  const lastNameValue = lastName.value.trim();
+
+  if (lastNameValue === '') {
+    return false;
+  } else {
+    console.log(lastNameValue);
+    return true;
+  }
+};
+
+const checkEmail = () => {
+  const emailValue = email.value.trim();
+
+  if (emailValue === '') {
+    return false;
+  } else {
+    console.log(emailValue);
+    return true;
+  }
+};
+
+// FIXME: the value of this input does'nt log in the console
+const checkMessage = () => {
+  const messageValue = message.value.trim();
+
+  if (messageValue === '') {
+    return false;
+  } else {
+    console.log(messageValue);
+    return true;
+  }
+};
+
+form.addEventListener('submit', e => {
+  if (
+    checkFirstName() == false ||
+    checkLastName() == false ||
+    checkEmail() == false ||
+    checkMessage == false
+  ) {
+    e.preventDefault();
+    console.log('Un ou plusieurs champs sont vides !');
+  } else {
+    e.preventDefault();
+  }
+});
